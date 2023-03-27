c2ee3ffeb5e9294d9f1a0708933463db9e3e280e
Yash Masani <yash.masani@gmail.com>
2023-03-27 10:34:29 -0400

    added excel parse

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod file;
+
+pub fn sum() -> u8 {
+    1 + 1
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use lib::*;
+use lib::file::parse_xls;
+
+fn main() {
+    println!("Hello, world!");
+    parse_xls().unwrap();
+
+}
